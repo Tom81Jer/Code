b60aac3e8506668ee6fd73e3b1000b138bcbef46
Tom81Jer <tom81jer@gmail.com>
2025-07-16 11:57:56 -0500

    Update Full-Backup-Size-Duration.sql

--- Full-Backup-Size-Duration.sql
+++ Full-Backup-Size-Duration.sql
@@ -1,19 +1,42 @@
+WITH LatestFullBackup AS (
+    SELECT
+        database_name,
+        MAX(backup_finish_date) AS latest_finish
+    FROM msdb.dbo.backupset
+    WHERE type = 'D'
+      AND database_name NOT IN ('master', 'model', 'msdb', 'tempdb', 'distribution')
+    GROUP BY database_name
+),
+BackupDetails AS (
+    SELECT
+        bs.database_name,
+        bs.backup_set_id,
+        bs.backup_start_date,
+        bs.backup_finish_date,
+        DATEDIFF(MINUTE, bs.backup_start_date, bs.backup_finish_date) AS duration_minutes,
+        CEILING(bs.backup_size / 1024.0 / 1024.0 / 1024.0) AS total_backup_size_gb,
+        CEILING(bs.compressed_backup_size / 1024.0 / 1024.0 / 1024.0) AS total_compressed_size_gb
+    FROM msdb.dbo.backupset bs
+    JOIN LatestFullBackup lfb
+      ON bs.database_name = lfb.database_name
+     AND bs.backup_finish_date = lfb.latest_finish
+    WHERE bs.type = 'D'
+),
+BackupFiles AS (
+    SELECT
+        bmf.media_set_id,
+        STRING_AGG(bmf.physical_device_name, ', ') AS backup_files
+    FROM msdb.dbo.backupmediafamily bmf
+    GROUP BY bmf.media_set_id
+)
 SELECT
-    bs.database_name,
-    MAX(bs.backup_start_date) AS backup_start_date,
-    MAX(bs.backup_finish_date) AS backup_finish_date,
-    DATEDIFF(MINUTE, MAX(bs.backup_start_date), MAX(bs.backup_finish_date)) AS duration_minutes,
-    CEILING(SUM(bs.backup_size) / 1024.0 / 1024.0 / 1024.0) AS total_backup_size_gb,
-    CEILING(SUM(bs.compressed_backup_size) / 1024.0 / 1024.0 / 1024.0) AS total_compressed_size_gb,
-    STRING_AGG(bmf.physical_device_name, ', ') AS backup_files
-FROM msdb.dbo.backupset bs
-JOIN msdb.dbo.backupmediafamily bmf
-    ON bs.media_set_id = bmf.media_set_id
-WHERE bs.type = 'D'
-  AND bs.database_name NOT IN ('master', 'model', 'msdb', 'tempdb', 'distribution')
-  AND bs.backup_finish_date = (
-      SELECT MAX(backup_finish_date)
-      FROM msdb.dbo.backupset bs2
-      WHERE bs2.database_name = bs.database_name AND bs2.type = 'D'
-  )
-GROUP BY bs.database_name;
+    bd.database_name,
+    bd.backup_start_date,
+    bd.backup_finish_date,
+    bd.duration_minutes,
+    bd.total_backup_size_gb,
+    bd.total_compressed_size_gb,
+    bf.backup_files
+FROM BackupDetails bd
+JOIN msdb.dbo.backupset bs ON bd.backup_set_id = bs.backup_set_id
+JOIN BackupFiles bf ON bs.media_set_id = bf.media_set_id;
