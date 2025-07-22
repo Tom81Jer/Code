ccb4255aee74bfd3bca6a350f4256aced6b3c269
Tom81Jer <tom81jer@gmail.com>
2025-07-22 02:05:29 -0500

    Update Uninstall SSMS Registry V2.ps1

--- Uninstall SSMS Registry V2.ps1
+++ Uninstall SSMS Registry V2.ps1
@@ -3,15 +3,16 @@ Invoke-Command -ComputerName "RemotePC" -ScriptBlock {
         "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
         "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
     )
-    $ssms = "Microsoft SQL Server Management Studio"
+    $filter = "Microsoft SQL Server Management Studio"
 
     foreach ($path in $paths) {
         Get-ChildItem $path | ForEach-Object {
             $props = Get-ItemProperty $_.PSPath
-            if ($props.DisplayName -like "$ssms*") {
+            if ($props.DisplayName -like "*$filter*") {
                 $uninstallCmd = $props.UninstallString
-                if ($props.UninstallString) {                    
-                    Start-Process $($props.UninstallString)  -Wait -Verb RunAs
+                if ($uninstallCmd) {
+                    $exe = $uninstallCmd -replace "/uninstall", ""
+                    Start-Process $exe -ArgumentList "/uninstall /quiet /norestart" -Wait -Verb RunAs
                 }
             }
         }
